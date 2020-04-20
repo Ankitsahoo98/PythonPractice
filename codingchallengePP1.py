fdcbe440d4b50780a450efd298219955876f3278
Ankitsahoo98 <63474379+Ankitsahoo98@users.noreply.github.com>
2020-04-20 19:35:39 +0530

    Add files via upload

---
+++ codingchallengePP1.py
@@ -0,0 +1,17 @@
+# 1st message
+#print('Why won't this line of code print')
+print ("this won't be a problem if we put double quotes")
+
+#2nd message
+#prnit('This line fails too!')
+print ('You need to check your spell')
+
+#3rd message
+#print "I think how to fix this one"
+print("You have to have balls to fix this")
+
+#4th message
+#input ('Please tell your name: ')
+#print(name)
+name=input('Assign your NAME: ')
+print(name)
